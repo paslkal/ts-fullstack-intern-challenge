eb37ff6c508b4d86c740f14412022ade285ddae0
paslkal <paskalsinda@gmail.com>
2024-09-08 23:34:15 +0300

    change active link

--- front/src/Header.tsx
+++ front/src/Header.tsx
@@ -1,11 +1,33 @@
+import { useState } from 'react'
 import './styles/Header.css'
 
+type active = 'all-cats' | 'liked-cats'
+
 export default function Header() {
+  const [active, setActive] = useState<active>('all-cats')
+
+  const handleClick = (newActive: active) => {
+    setActive(newActive)
+  }
+
   return (
     <header>
       <nav>
-        <a href="#" className="active">Все котики</a>
-        <a href="#" className="not-active">Любимые котики</a>
+        <a 
+          href="#" 
+          className={active === 'all-cats' ? 'active' : 'not-active'}
+          onClick={() => handleClick('all-cats')}
+        >
+          Все котики
+        </a>
+        
+        <a 
+          href="#" 
+          className={active === 'liked-cats' ? 'active' : 'not-active'}
+          onClick={() => handleClick('liked-cats')}
+        >
+          Любимые котики
+        </a>
       </nav>
     </header>
   )
