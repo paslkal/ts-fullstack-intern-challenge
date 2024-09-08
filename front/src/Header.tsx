d13cd54652727038585b8443fd17a569c445b15f
paslkal <paskalsinda@gmail.com>
2024-09-08 20:42:19 +0300

    create header

---
+++ front/src/Header.tsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="#" className="active">Все котики</a>
+        <a href="#" className="not-active">Любимые котики</a>
+      </nav>
+    </header>
+  )
+}
