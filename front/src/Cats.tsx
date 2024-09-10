23a785a5710b66520b7b9886071d3c387f7a805f
paslkal <paskalsinda@gmail.com>
2024-09-10 12:01:48 +0300

    add or delete cat from backend when clicking heart

--- front/src/Cats.tsx
+++ front/src/Cats.tsx
@@ -3,9 +3,11 @@ import './styles/Cats.css'
 import heart from './assets/heart.svg'
 import Cat from "./interfaces/cat.interface"
 import { backendURL, catAPIUrl } from "./url"
+import NewCat from "./interfaces/newCat.interface"
+import clickedHeart from './assets/clicked-heart.svg'
 
 export default function Cats() {
-  const [cats, setCats] = useState<Cat[]>([])
+  const [cats, setCats] = useState<NewCat[]>([])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,7 +16,13 @@ export default function Cats() {
   
         const fetchedCats: Cat[] = await response.json()
   
-        setCats(fetchedCats)        
+        const newCats: NewCat[] = fetchedCats.map((cat) => {
+          const newCat = {...cat, isLiked: false}
+
+          return newCat
+        })
+
+        setCats(newCats)        
       } catch (error) {
         console.error(error)
       }
@@ -23,18 +31,48 @@ export default function Cats() {
     fetchData()
   }, [])
 
-  const handleClick = (cat_id: string) => {
-    try {
-      fetch(`${backendURL}/likes`, {
-        method: 'POST',
-        body: JSON.stringify({cat_id}),
-        headers: {"Content-Type": "application/json"}
-      })
+  const handleClick = (catId: string) => {
+    const cat = cats.find(cat => cat.id === catId)
+    
+    if (!cat?.isLiked) {
+      console.log(cat)
 
+      try {
+        fetch(`${backendURL}/likes`, {
+          method: 'POST',
+          body: JSON.stringify({cat_id: catId}),
+          headers: {"Content-Type": "application/json"}
+        })
+  
+        changeLike(catId, true)
+
+      } catch (error) {
+        console.error(error)
+      }
+    } else {
+      try {
+        fetch(`${backendURL}/likes/${catId}`, {method: 'DELETE'})
+        
+        changeLike(catId, false)
+      } catch (error) {
+        console.error(error);
+      }
+      
       console.log('click')
-    } catch (error) {
-      console.error(error)
     }
+    
+  }
+
+  const changeLike = (catId: string, isLiked: boolean) => {
+    const changedCats = cats.map(cat => {
+      if (cat.id === catId) {
+        return {...cat, isLiked: isLiked}
+      }
+
+      return cat
+    })
+
+    setCats(changedCats)
   }
 
   return (
@@ -48,7 +86,7 @@ export default function Cats() {
               <div className="cat-container" key={cat.id}>
                 <img src={url} alt="cat" className="cat-image"/>
                 <img 
-                  src={heart} 
+                  src={cat.isLiked ? clickedHeart : heart} 
                   alt="heart" 
                   className="heart-image"
                   onClick={() => handleClick(cat.id)}
