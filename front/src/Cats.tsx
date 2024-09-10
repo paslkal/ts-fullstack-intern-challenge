3561358adab731fa427570105f5ffde6762264e8
paslkal <paskalsinda@gmail.com>
2024-09-10 03:13:58 +0300

    load images on 'Любимые котики' page from postgres database

--- front/src/Cats.tsx
+++ front/src/Cats.tsx
@@ -1,24 +1,22 @@
 import { useEffect, useState } from "react"
 import './styles/Cats.css'
 import heart from './assets/heart.svg'
-
-interface Cat{
-  id: string,
-  height: number,
-  width: number,
-  url: string
-}
+import Cat from "./interfaces/cat.interface"
 
 export default function Cats() {
   const [cats, setCats] = useState<Cat[]>([])
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(' https://api.thecatapi.com/v1/images/search?limit=10')
-
-      const fetchedCats: Cat[] = await response.json()
-
-      setCats(fetchedCats)
+      try {
+        const response = await fetch(' https://api.thecatapi.com/v1/images/search?limit=10')
+  
+        const fetchedCats: Cat[] = await response.json()
+  
+        setCats(fetchedCats)        
+      } catch (error) {
+        console.error(error)
+      }
     }
 
     fetchData()
--- front/src/LikedCats.tsx
+++ front/src/LikedCats.tsx
@@ -1,7 +1,57 @@
+import { useEffect, useState } from 'react'
+import heart from './assets/heart.svg'
+import './styles/Cats.css'
+import CatInfo from './interfaces/catInfo.interface'
+import Cat from './interfaces/cat.interface'
+
 export default function LikedCats() {
+  const [cats, setCats] = useState<Cat[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = 'http://localhost:3000/likes'
+        const response = await fetch(url)
+  
+        const fetchedData: CatInfo[] = await response.json()
+
+        const catsPromises = fetchedData.map(async (catInfo): Promise<Cat> => {
+          const {cat_id} = catInfo
+
+          const response = await fetch(`https://api.thecatapi.com/v1/images/${cat_id}`) 
+
+          const cat: Cat = await response.json()
+
+          return cat
+        })
+  
+        const fetchedCats = await Promise.all(catsPromises)
+
+        setCats(fetchedCats)        
+      } catch (error) {
+        console.error(error)        
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
-    <>
-      Liked Cats
-    </>
+    <main>
+      <div className="cats-grid">
+        {
+          cats.map((cat) => {
+            const {url} = cat
+
+            return(
+              <div className="cat-container">
+                <img src={url} alt="cat" className="cat-image"/>
+                <img src={heart} alt="heart" className="heart-image"/>
+              </div>
+            )
+          })
+        }
+      </div>
+    </main>
   )
 }
